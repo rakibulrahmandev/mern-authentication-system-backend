62d56b11c5e390573cf061837f70463322ef7bc0
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-10-02 21:19:09 +0600

    create auth.signup.controller.js file and create basic controller stucture

---
+++ controllers/auth.signup.controller.js
@@ -0,0 +1,10 @@
+// import modules ------------------------------------------------------------------>
+import userSchema from '../models/auth.user.model.js';
+
+// create controller --------------------------------------------------------------->
+const sign_up = async (req, res) => {
+    
+};
+
+// export module ------------------------------------------------------------------->
+export default sign_up;
