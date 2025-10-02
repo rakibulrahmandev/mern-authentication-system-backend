c5cf6bdc4782c5f269532653416ed21f76ae4d37
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-10-02 22:04:45 +0600

    added token expires date

--- utils/generate.jwt.token.js
+++ utils/generate.jwt.token.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 // generate function --------------------------------------------------------------->
 const generateJwtToken = (id, email) => {
-    const jwt_token = jwt.sign({ id, email }, process.env.JWT_SECRET);
+    const jwt_token = jwt.sign({ id, email }, process.env.JWT_SECRET, { expiresIn: '7d' });
     return jwt_token;
 };
 
