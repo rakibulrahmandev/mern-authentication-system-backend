07ed5d0113c01e850a13b7b8df65478f20ca4aac
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-10-02 23:32:17 +0600

    added dotenv configaration in resend.config.js file

--- config/resend.config.js
+++ config/resend.config.js
@@ -1,5 +1,9 @@
 // import modules ------------------------------------------------------------------>
 import { Resend } from "resend";
+import dotenv from 'dotenv';
+
+// dotenv config ------------------------------------------------------------------->
+dotenv.config();
 
 // resend config ------------------------------------------------------------------->
 const resend = new Resend(process.env.RESEND_API_KEY);
